c22abd5211f89c605412e63aa50d641287c664d6
Leandro <atiladevelop@gmail.com>
2024-03-07 22:17:27 -0300

    chore: put endpoint

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/app/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server';
+import * as yup from 'yup';
 
 interface Segments {
   params: {
@@ -26,4 +27,30 @@ export async function GET(request: Request, { params }: Segments) {
   }
 
   return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional(),
+});
+
+export async function PUT(req: Request, { params }: Segments) {
+  try {
+    const { id } = params;
+    const { description, complete } = await putSchema.validate(
+      await req.json()
+    );
+
+    const updateTodo = await prisma.todo.update({
+      where: { id },
+      data: {
+        description,
+        complete,
+      },
+    });
+
+    return NextResponse.json(updateTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
