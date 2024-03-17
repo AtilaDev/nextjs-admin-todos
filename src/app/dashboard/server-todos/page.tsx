618b50012ea2b05843ed1c8efb1a7f3bad023c47
Leandro <atiladevelop@gmail.com>
2024-03-17 14:11:29 -0300

    chore: working on server actions

---
+++ src/app/dashboard/server-todos/page.tsx
@@ -0,0 +1,21 @@
+import prisma from '@/app/lib/prisma';
+import { NewTodo, TodosGrid } from '@/todos';
+
+export const metadata = {
+  title: 'Listado de Todos',
+  description: 'Listado de Todos',
+};
+
+export default async function ServerTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
+  return (
+    <>
+      <span className='text-3xl mb-10'>Server Actions</span>
+      <div className='w-full px-3 mx-5 mb-5'>
+        <NewTodo />
+      </div>
+      <TodosGrid todos={todos} />
+    </>
+  );
+}
