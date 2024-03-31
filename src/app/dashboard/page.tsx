d0e447ee06a8f51c66b624c0af3f7e58b7a3b8ce
Leandro <atiladevelop@gmail.com>
2024-03-31 15:08:38 -0300

    chore: Working with Auth.js

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,24 @@
 import { WidgetItem } from '@/components';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
-    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-      {/* <WidgetItem /> */}
+    <div className='grid gap-6 grid-cols-1 sm:grid-cols-2'>
+      <WidgetItem title='Usuario conectado S-Side'>
+        <div className='flex flex-col'>
+          <span>{session.user?.name}</span>
+          <span>{session.user?.email}</span>
+          <span>{session.user?.image}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }
